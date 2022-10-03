8ca17df6362574b2728ece3798157c850d579f5b
vasilii88 <golevvasilii1988@gmail.com>
2022-10-03 12:37:36 +0700

    текст комментария

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.environ.get('SECRET_KEY', default='12345')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 

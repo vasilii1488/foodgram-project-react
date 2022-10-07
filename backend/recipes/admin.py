68dd25ebbe2aa553bea8930ede5e9acfcf37474b
vasilii88 <golevvasilii1988@gmail.com>
2022-10-07 15:33:14 +0700

    текст комментария

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import (Ingredient, Recipe, Tag, 
-                     RecipeIngredient, ShopList)
+                    RecipeIngredient, ShopList)
 
 
 class TagAdmin(admin.ModelAdmin):

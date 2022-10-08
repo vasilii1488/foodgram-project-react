86bffaefb1f08402c27fecb33d31b4ac9d5b697f
vasilii88 <golevvasilii1988@gmail.com>
2022-10-08 15:39:49 +0700

    текст комментария

--- backend/recipes/utils.py
+++ backend/recipes/utils.py
@@ -6,21 +6,31 @@ from .models import Recipe
 from .serializers import RecipeFollowSerializer
 
 
+# def remov_obj(model, user, pk):
+#     recipe = get_object_or_404(Recipe, id=pk)
+#     if not model.objects.filter(user=user, recipe__id=pk).exists():
+#         return Response('Рецепт отсутствует в избранном',
+#                         status=status.HTTP_400_BAD_REQUEST)
+#     obj = model.objects.get(user=user, recipe=recipe)
+#     obj.delete()
+#     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 def remov_obj(model, user, pk):
-    recipe = get_object_or_404(Recipe, id=pk)
-    if not model.objects.filter(user=user, recipe=recipe).exists():
-        return Response('Рецепт отсутствует в избранном',
-                        status=status.HTTP_400_BAD_REQUEST)
-    obj = model.objects.get(user=user, recipe=recipe)
-    obj.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    obj = model.objects.filter(user=user, recipe__id=pk)
+    if obj.exists():
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response({
+        'errors': 'Рецепт уже удален'
+    }, status=status.HTTP_400_BAD_REQUEST)
 
 
 def add_obj(model, user, pk):
     recipe = get_object_or_404(Recipe, id=pk)
-    if model.objects.filter(user=user, recipe=recipe).exists():
+    if model.objects.filter(user=user, recipe__id=pk).exists():
         return Response('Рецепт добавлен в список',
                         status=status.HTTP_400_BAD_REQUEST)
     obj = model.objects.create(user=user, recipe=recipe)
-    serializer = RecipeFollowSerializer(obj.recipe)
+    serializer = RecipeFollowSerializer(obj)
     return Response(serializer.data, status=status.HTTP_201_CREATED)

2e8ea944a1f3902aff9f14aeb192b0f3b1c56d30
vasilii88 <golevvasilii1988@gmail.com>
2022-11-09 22:49:25 +0700

    переписал избранное

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -116,11 +116,10 @@ class RecipeView(viewsets.ModelViewSet):
         model = Favorite
         return remov_obj(model=model, user=user, pk=pk)
 
-    @action(detail=False, url_path='shopping_cart', methods=['POST'],
+    @action(detail=True, url_path='shopping_cart', methods=['POST'],
             permission_classes=[IsAuthenticated])
     def recipe_cart(self, request, pk):
         """ Метод добавления рецепта в список покупок. """
-
         user = request.user
         model = ShopList
         return add_obj(model=model, user=user, pk=pk)

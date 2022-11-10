f2eb63f40d6982de018f411089a2d6d881a353bb
vasilii88 <golevvasilii1988@gmail.com>
2022-11-10 14:18:52 +0700

    переписал избранное

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -133,7 +133,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 6,
     'page_size_query_param': 'limit',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
